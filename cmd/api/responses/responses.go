f1ccafc8bb40f2a914b3bd9bfab0cb7bef4f5002
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-12 14:31:13 +0000

    feat: success and error json response models

---
+++ cmd/api/responses/responses.go
@@ -0,0 +1,32 @@
+package responses
+
+import (
+	"time"
+)
+
+type errorResponse struct {
+	Time    time.Duration `json:"duration"`
+	Message string        `json:"message"`
+}
+
+func Error(start time.Time, message string) errorResponse {
+	return errorResponse{
+		Time:    time.Duration(time.Since(start).Seconds()),
+		Message: message,
+	}
+}
+
+type successResponse struct {
+	Status   string        `json:"status"`
+	Duration time.Duration `json:"duration"`
+	Data     interface{}   `json:"data"`
+}
+
+func Success(start time.Time, data interface{}) *successResponse {
+
+	return &successResponse{
+		Status:   "success",
+		Duration: time.Duration(time.Since(start).Seconds()),
+		Data:     data,
+	}
+}
