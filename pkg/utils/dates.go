8ee081ec7365fae99b0c2cd57274199d7b1d1114
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-12 14:38:36 +0000

    feat: ubisoft request date function

---
+++ pkg/utils/dates.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+func getDate(day int) string {
+	year, month, day := time.Now().AddDate(0, 0, day).Date()
+	return fmt.Sprintf("%v%02d%02d", year, int(month), day)
+}
