e5ba6b8445cce5cd691a39f211437f88885aa4a2
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-12 16:36:43 +0000

    fix: return pointer to ubisoft profile response

--- pkg/ubisoft/requests.go
+++ pkg/ubisoft/requests.go
@@ -4,19 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"time"
 
 	"github.com/cloudwego/hertz/pkg/app/client"
 	"github.com/cloudwego/hertz/pkg/protocol"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
-	"github.com/eliassebastian/r6index-api/cmd/api/models"
 	"github.com/eliassebastian/r6index-api/pkg/auth"
 	ubisoft "github.com/eliassebastian/r6index-api/pkg/ubisoft/models"
 )
 
 //import "fmt"
 
-func GetPlayerProfile(ctx context.Context, client client.Client, auth *auth.UbisoftSession, output *models.Player, name, uuid, platform string) error {
+func GetPlayerProfile(ctx context.Context, client client.Client, auth *auth.UbisoftSession, name, uuid, platform string) (*ubisoft.Profile, error) {
 
 	req := protocol.AcquireRequest()
 	res := protocol.AcquireResponse()
@@ -29,24 +27,18 @@ func GetPlayerProfile(ctx context.Context, client client.Client, auth *auth.Ubis
 
 	err := client.DoRedirects(ctx, req, res, 1)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var profile ubisoft.ProfileModel
 	de := json.NewDecoder(res.BodyStream()).Decode(&profile)
 	if de != nil {
-		return errors.New("error decoding response")
+		return nil, errors.New("error decoding response")
 	}
 
 	if len(profile.Profiles) == 0 {
-		return errors.New("profile not found")
+		return nil, errors.New("profile not found")
 	}
 
-	output.ProfileId = profile.Profiles[0].ProfileID
-	output.UserId = profile.Profiles[0].UserID
-	output.Nickname = profile.Profiles[0].NameOnPlatform
-	output.Platform = profile.Profiles[0].PlatformType
-	output.LastUpdate = time.Now().UTC()
-
-	return nil
+	return &profile.Profiles[0], nil
 }
