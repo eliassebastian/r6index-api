283be3ff5602caf6c10dc6728979ab86b781cfed
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-13 15:15:20 +0000

    feat: alias cache struct

--- cmd/api/models/player.go
+++ cmd/api/models/player.go
@@ -2,10 +2,22 @@ package models
 
 import "time"
 
+type AliasCache struct {
+	Name string
+}
+
+type Alias struct {
+	Name string    `json:"name"`
+	Date time.Time `json:"date"`
+}
+
 type Player struct {
 	ProfileId  string    `json:"profileId"`
 	UserId     string    `json:"userId"`
 	Platform   string    `json:"platform"`
 	Nickname   string    `json:"nickname"`
 	LastUpdate time.Time `json:"lastUpdate"`
+	Aliases    *[]Alias  `json:"aliases"`
+	Xp         int32     `json:"xp"`
+	Level      int16     `json:"level"`
 }
