c334b96269fc22efe72a75675cbc7463e0571bd3
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-13 15:16:16 +0000

    feat: xp + playtime uri

--- pkg/ubisoft/url.go
+++ pkg/ubisoft/url.go
@@ -9,3 +9,13 @@ func profileUri(name, uuid, platform string) string {
 
 	return fmt.Sprintf("https://public-ubiservices.ubi.com/v3/users/%s/profiles?platformType=%s}", uuid, platform)
 }
+
+func playtimeUri(uuid string) string {
+
+	return fmt.Sprintf("https://public-ubiservices.ubi.com/v1/profiles/stats?profileIds=%s&spaceId=0d2ae42d-4c27-4cb7-af6c-2099062302bb&statNames=PPvPTimePlayed,PPvETimePlayed,PTotalTimePlayed,PClearanceLevel", uuid)
+
+}
+
+func xpUri(uuid string) string {
+	return fmt.Sprintf("https://public-ubiservices.ubi.com/v1/spaces/0d2ae42d-4c27-4cb7-af6c-2099062302bb/title/r6s/rewards/public_profile?profile_id=%s", uuid)
+}
