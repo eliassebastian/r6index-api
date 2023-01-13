a1c38739d6982e5b5e03ce5438c24b6b9de7c033
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-13 15:15:05 +0000

    feat: redis cache

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -1,6 +1,9 @@
 package cache
 
 import (
+	"context"
+	"time"
+
 	"github.com/go-redis/cache/v8"
 	"github.com/go-redis/redis/v8"
 )
@@ -10,11 +13,17 @@ type CacheStore struct {
 	cache *cache.Cache
 }
 
-func New() (*CacheStore, error) {
-
+func New(ctx context.Context) (*CacheStore, error) {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "172.0.0.1:6379",
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
+
+	err := rdb.Ping(ctx).Err()
+	if err != nil {
+		return nil, err
+	}
 
 	cdb := cache.New(&cache.Options{
 		Redis: rdb,
@@ -24,4 +33,35 @@ func New() (*CacheStore, error) {
 		redis: rdb,
 		cache: cdb,
 	}, nil
+}
+
+func (c *CacheStore) Once(key string, set interface{}, obj interface{}) error {
+	return c.cache.Once(&cache.Item{
+		Key:   key,
+		Value: set,
+		Do: func(*cache.Item) (interface{}, error) {
+			return obj, nil
+		},
+	})
+}
+
+// func (c *CacheStore) Set(ctx context.Context, key string, value interface{}) error {
+// 	return c.cache.Set(&cache.Item{
+// 		Ctx:   ctx,
+// 		Key:   key,
+// 		Value: value,
+// 		//TTL:   time.Hour,
+// 	})
+// }
+
+func (c *CacheStore) Set(ctx context.Context, key, value string, ttl time.Duration) error {
+	return c.redis.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *CacheStore) Get(ctx context.Context, key string, obj interface{}) (string, error) {
+	return c.redis.Get(ctx, key).Result()
+}
+
+func (c *CacheStore) Close() {
+	c.redis.Close()
 }
