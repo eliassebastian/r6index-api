e72eb55e1ee454a8a2534800293eace26bf84a12
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-15 20:01:46 +0000

    fix: fetch seasons 5-27

--- pkg/ubisoft/url.go
+++ pkg/ubisoft/url.go
@@ -28,5 +28,5 @@ func rankedOneUri(uuid, platform string, xplay bool) string {
 	spaceId := PlatformSpaceId[platform]
 	sandboxId := PlatformSandboxId[platform]
 
-	return fmt.Sprintf("https://public-ubiservices.ubi.com/v1/spaces/%s/sandboxes/%s/r6karma/player_skill_records?board_ids=pvp_ranked&region_ids=emea,ncsa,apac,global&season_ids=-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22,-23,-24,-25,-26,-27&profile_ids=%s", spaceId, sandboxId, uuid)
+	return fmt.Sprintf("https://public-ubiservices.ubi.com/v1/spaces/%s/sandboxes/%s/r6karma/player_skill_records?board_ids=pvp_ranked&region_ids=emea,ncsa,apac,global&season_ids=-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22&profile_ids=%s", spaceId, sandboxId, uuid)
 }
