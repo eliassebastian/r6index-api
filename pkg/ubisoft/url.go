70b04155ce79d62ebd1e0ea65bab0fe90b3dc50d
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-16 11:47:31 +0000

    feat: ranked two uri

--- pkg/ubisoft/url.go
+++ pkg/ubisoft/url.go
@@ -30,3 +30,9 @@ func rankedOneUri(uuid, platform string, xplay bool) string {
 
 	return fmt.Sprintf("https://public-ubiservices.ubi.com/v1/spaces/%s/sandboxes/%s/r6karma/player_skill_records?board_ids=pvp_ranked&region_ids=emea,ncsa,apac,global&season_ids=-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20,-21,-22&profile_ids=%s", spaceId, sandboxId, uuid)
 }
+
+func rankedTwoUri(uuid, platform string) string {
+	platform = PlatformRankedTwo[platform]
+
+	return fmt.Sprintf("https://public-ubiservices.ubi.com/v2/spaces/0d2ae42d-4c27-4cb7-af6c-2099062302bb/title/r6s/skill/full_profiles?profile_ids=%s&platform_families=%s", uuid, platform)
+}
