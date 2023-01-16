1ed2ce73bba8a0aae7a37db76133bb55164b1def
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-16 11:48:39 +0000

    feat: ranked two info added to api response

--- cmd/api/models/player.go
+++ cmd/api/models/player.go
@@ -16,13 +16,14 @@ type Alias struct {
 }
 
 type Player struct {
-	ProfileId  string                       `json:"profileId"`
-	UserId     string                       `json:"userId"`
-	Platform   string                       `json:"platform"`
-	Nickname   string                       `json:"nickname"`
-	LastUpdate time.Time                    `json:"lastUpdate"`
-	Aliases    *[]Alias                     `json:"aliases"`
-	Xp         int32                        `json:"xp"`
-	Level      int16                        `json:"level"`
-	RankedOne  *[]ubisoft.RankedOutputModel `json:"rankedOne"`
+	ProfileId  string                          `json:"profileId"`
+	UserId     string                          `json:"userId"`
+	Platform   string                          `json:"platform"`
+	Nickname   string                          `json:"nickname"`
+	LastUpdate time.Time                       `json:"lastUpdate"`
+	Aliases    *[]Alias                        `json:"aliases"`
+	Xp         int32                           `json:"xp"`
+	Level      int16                           `json:"level"`
+	RankedOne  *[]ubisoft.RankedOutputModel    `json:"rankedOne"`
+	RankedTwo  *[]ubisoft.RankedTwoOutputModel `json:"rankedTwo"`
 }
