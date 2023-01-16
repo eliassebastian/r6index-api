8e3b46b4d6749c59230ca01d106dd406c2db4654
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-16 11:46:57 +0000

    feat: ranked two model

---
+++ pkg/ubisoft/models/rankedtwo.go
@@ -0,0 +1,60 @@
+package ubisoft
+
+type RankedTwoOutputModel struct {
+	MaxRank         int16  `json:"max_rank"`
+	SeasonID        int16  `json:"season"`
+	Rank            int16  `json:"rank"`
+	MaxRankPoints   int    `json:"max_rank_points"`
+	RankPoints      int    `json:"rank_points"`
+	TopRankPosition int    `json:"top_rank_position"`
+	Abandons        int    `json:"abandons"`
+	Losses          int    `json:"losses"`
+	Wins            int    `json:"wins"`
+	Deaths          int    `json:"deaths"`
+	Kills           int    `json:"kills"`
+	RankText        string `json:"rank_text"`
+	MaxRankText     string `json:"max_rank_text"`
+}
+
+type RankedTwoModel struct {
+	PlatformFamiliesFullProfiles []RankedTwoPlatformFamilies `json:"platform_families_full_profiles"`
+}
+
+type RankedTwoPlatformFamilies struct {
+	BoardIdsFullProfiles []RankedTwoBoards `json:"board_ids_full_profiles"`
+	PlatformFamily       string            `json:"platform_family"`
+}
+
+type RankedTwoBoards struct {
+	BoardID      string                 `json:"board_id"`
+	FullProfiles []RankedTwoFullProfile `json:"full_profiles"`
+}
+
+type RankedTwoFullProfile struct {
+	Profile          RankedTwoProfile          `json:"profile"`
+	SeasonStatistics RankedTwoSeasonStatistics `json:"season_statistics"`
+}
+
+type RankedTwoProfile struct {
+	BoardID         string `json:"board_id"`
+	ID              string `json:"id"`
+	MaxRank         int16  `json:"max_rank"`
+	MaxRankPoints   int    `json:"max_rank_points"`
+	PlatformFamily  string `json:"platform_family"`
+	Rank            int16  `json:"rank"`
+	RankPoints      int    `json:"rank_points"`
+	SeasonID        int16  `json:"season_id"`
+	TopRankPosition int    `json:"top_rank_position"`
+}
+
+type RankedTwoSeasonStatistics struct {
+	Deaths        int                    `json:"deaths"`
+	Kills         int                    `json:"kills"`
+	MatchOutcomes RankedTwoMatchOutcomes `json:"match_outcomes"`
+}
+
+type RankedTwoMatchOutcomes struct {
+	Abandons int `json:"abandons"`
+	Losses   int `json:"losses"`
+	Wins     int `json:"wins"`
+}
