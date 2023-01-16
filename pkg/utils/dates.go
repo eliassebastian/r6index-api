9dea369be77bfd3f4f36a6104e1728e69d0ba532
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-16 16:39:55 +0000

    fix: export getDate package function

--- pkg/utils/dates.go
+++ pkg/utils/dates.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func getDate(day int) string {
+func GetDate(day int) string {
 	year, month, day := time.Now().AddDate(0, 0, day).Date()
 	return fmt.Sprintf("%v%02d%02d", year, int(month), day)
 }
