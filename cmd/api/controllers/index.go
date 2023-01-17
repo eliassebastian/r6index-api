6a4620fb910d8f06c943532c1374582a8f2a63ed
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-17 11:55:37 +0000

    feat: add weapon, maps, operator requests to group

--- cmd/api/controllers/index.go
+++ cmd/api/controllers/index.go
@@ -100,6 +100,30 @@ func (ic *IndexController) RequestHandler(ctx context.Context, c *app.RequestCon
 		return err
 	})
 
+	group.Go(func() error {
+		w, err := ubisoft.GetWeapons(ctx, *ic.client, us, profile.ProfileID, platform, true)
+
+		output.Weapons = w
+
+		return err
+	})
+
+	group.Go(func() error {
+		w, err := ubisoft.GetMaps(ctx, *ic.client, us, profile.ProfileID, platform, true)
+
+		output.Maps = w
+
+		return err
+	})
+
+	group.Go(func() error {
+		w, err := ubisoft.GetOperators(ctx, *ic.client, us, profile.ProfileID, platform, true)
+
+		output.Operators = w
+
+		return err
+	})
+
 	if err := group.Wait(); err != nil {
 		c.JSON(consts.StatusBadRequest, responses.Error(startTime, err.Error()))
 		return
