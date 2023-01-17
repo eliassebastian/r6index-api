7da6b0d1d2dfd2b3e461efc91d43d7e5db201344
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-17 13:36:21 +0000

    feat: summary request goroutine

--- cmd/api/controllers/index.go
+++ cmd/api/controllers/index.go
@@ -132,6 +132,14 @@ func (ic *IndexController) RequestHandler(ctx context.Context, c *app.RequestCon
 		return err
 	})
 
+	group.Go(func() error {
+		w, err := ubisoft.GetSummary(ctx, *ic.client, us, profile.ProfileID, platform, true)
+
+		output.Summary = w
+
+		return err
+	})
+
 	if err := group.Wait(); err != nil {
 		c.JSON(consts.StatusBadRequest, responses.Error(startTime, err.Error()))
 		return
