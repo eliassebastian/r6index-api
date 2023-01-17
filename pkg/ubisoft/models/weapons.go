db42304fe8b94125ae3a16408a3c472ba6a90f78
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-17 11:52:51 +0000

    feat: weapons struct

---
+++ pkg/ubisoft/models/weapons.go
@@ -0,0 +1,50 @@
+package ubisoft
+
+type WeaponsModel struct {
+	GameModes GameModes `json:"gameModes"`
+}
+
+type GameModes struct {
+	Ranked Ranked `json:"ranked"`
+}
+
+type Ranked struct {
+	Type      string          `json:"type"`
+	TeamRoles WeaponTeamRoles `json:"teamRoles"`
+}
+
+type WeaponTeamRoles struct {
+	All      WeaponTeamRole `json:"all"`
+	Attacker WeaponTeamRole `json:"attacker"`
+	Defender WeaponTeamRole `json:"defender"`
+}
+
+type WeaponTeamRole struct {
+	WeaponSlots WeaponSlots `json:"weaponSlots"`
+}
+
+type WeaponSlots struct {
+	SecondaryWeapons WeaponType `json:"secondaryWeapons"`
+	PrimaryWeapons   WeaponType `json:"primaryWeapons"`
+}
+
+type WeaponType struct {
+	WeaponTypes []WeaponTypes `json:"weaponTypes"`
+}
+
+type WeaponTypes struct {
+	WeaponType string    `json:"weaponType"`
+	Weapons    []Weapons `json:"weapons"`
+}
+
+type Weapons struct {
+	WeaponName          string  `json:"weaponName"`
+	RoundsPlayed        int     `json:"roundsPlayed"`
+	RoundsWon           int     `json:"roundsWon"`
+	RoundsLost          int     `json:"roundsLost"`
+	Kills               int     `json:"kills"`
+	Headshots           int     `json:"headshots"`
+	HeadshotAccuracy    float64 `json:"headshotAccuracy"`
+	RoundsWithAKill     float64 `json:"roundsWithAKill"`
+	RoundsWithMultiKill float64 `json:"roundsWithMultiKill"`
+}
