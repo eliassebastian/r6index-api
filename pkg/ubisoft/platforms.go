40cdaa5e35fbdbafeae485553b11869d0bba40fa
Elias Sebastian <eeschmoelz@gmail.com>
2023-01-17 11:54:31 +0000

    fix: elements to platformspaceid for modern api

--- pkg/ubisoft/platforms.go
+++ pkg/ubisoft/platforms.go
@@ -1,17 +1,21 @@
 package ubisoft
 
 var PlatformSpaceId = map[string]string{
-	"uplay": "5172a557-50b5-4665-b7db-e3f2e8c5041d",
-	"psn":   "05bfb3f7-6c21-4c42-be1f-97a33fb5cf66",
-	"xbl":   "98a601e5-ca91-4440-b1c5-753f601a2c90",
-	"xplay": "0d2ae42d-4c27-4cb7-af6c-2099062302bb",
+	"uplay":   "5172a557-50b5-4665-b7db-e3f2e8c5041d",
+	"psn":     "05bfb3f7-6c21-4c42-be1f-97a33fb5cf66",
+	"xbl":     "98a601e5-ca91-4440-b1c5-753f601a2c90",
+	"xplay":   "0d2ae42d-4c27-4cb7-af6c-2099062302bb",
+	"PC":      "5172a557-50b5-4665-b7db-e3f2e8c5041d",
+	"CONSOLE": "0d2ae42d-4c27-4cb7-af6c-2099062302bb",
 }
 
 var PlatformSandboxId = map[string]string{
-	"uplay": "OSBOR_PC_LNCH_A",
-	"psn":   "OSBOR_PS4_LNCH_A",
-	"xbl":   "OSBOR_XBOXONE_LNCH_A",
-	"xplay": "OSBOR_XPLAY_LNCH_A",
+	"uplay":   "OSBOR_PC_LNCH_A",
+	"psn":     "OSBOR_PS4_LNCH_A",
+	"xbl":     "OSBOR_XBOXONE_LNCH_A",
+	"xplay":   "OSBOR_XPLAY_LNCH_A",
+	"PC":      "OSBOR_PC_LNCH_A",
+	"CONSOLE": "OSBOR_XPLAY_LNCH_A",
 }
 
 var PlatformGameId = map[string]string{
