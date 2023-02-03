5afe16b7bcebd2d2b781c984799f98ad6f161e79
Elias Sebastian <eeschmoelz@gmail.com>
2023-02-03 17:32:10 +0000

    fix: unix time data type

--- cmd/api/models/player.go
+++ cmd/api/models/player.go
@@ -16,6 +16,11 @@ type Alias struct {
 	Date time.Time `json:"date"`
 }
 
+type ProfileCache struct {
+	LastUpdate int64
+	Aliases    *[]Alias
+}
+
 type PlayerFound struct {
 	Id string `json:"profileId"`
 }
@@ -26,8 +31,24 @@ type Player struct {
 	UserId     string                          `json:"userId"`
 	Platform   string                          `json:"platform"`
 	Nickname   string                          `json:"nickname"`
-	LastUpdate time.Time                       `json:"lastUpdate"`
+	LastUpdate int64                           `json:"lastUpdate"`
 	Aliases    *[]Alias                        `json:"aliases"`
+	Xp         int32                           `json:"xp"`
+	Level      int16                           `json:"level"`
+	Summary    *ubisoft.SummaryTeamRoles       `json:"summary"`
+	RankedOne  *[]ubisoft.RankedOutputModel    `json:"rankedOne"`
+	RankedTwo  *[]ubisoft.RankedTwoOutputModel `json:"rankedTwo"`
+	Weapons    *ubisoft.WeaponTeamRoles        `json:"weapons"`
+	Maps       *ubisoft.MapsTeamRoles          `json:"maps"`
+	Operators  *ubisoft.OperatorTeamRoles      `json:"operators"`
+	Trends     *ubisoft.TrendOutput            `json:"trends"`
+}
+
+type PlayerUpdate struct {
+	ProfileId  string                          `json:"profileId"`
+	Nickname   string                          `json:"nickname,omitempty"`
+	LastUpdate int64                           `json:"lastUpdate"`
+	Aliases    *[]Alias                        `json:"aliases,omitempty"`
 	Xp         int32                           `json:"xp"`
 	Level      int16                           `json:"level"`
 	Summary    *ubisoft.SummaryTeamRoles       `json:"summary"`
