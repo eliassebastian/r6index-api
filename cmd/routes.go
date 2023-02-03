2fdb6941aaa20a8ad9aad8c236821c30bdd7dc01
Elias Sebastian <eeschmoelz@gmail.com>
2023-02-03 17:32:50 +0000

    feat: update,search, player endpoints

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"context"
-	"time"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/eliassebastian/r6index-api/cmd/api/controllers"
-	"github.com/eliassebastian/r6index-api/cmd/api/responses"
-	"github.com/eliassebastian/r6index-api/pkg/utils"
 )
 
 func routes(s *server.Hertz, sc *serverConfig) {
 	ic := controllers.NewIndexController(sc.Authentication, sc.Client, sc.Cache, sc.DB)
+	uc := controllers.NewUpdateController(sc.Authentication, sc.Client, sc.Cache, sc.DB)
+	pc := controllers.NewPlayerController(sc.Cache, sc.DB)
+	ssc := controllers.NewSearchController(sc.DB)
 
 	h := s.Group("/v1")
 
@@ -22,36 +22,8 @@ func routes(s *server.Hertz, sc *serverConfig) {
 		c.String(consts.StatusOK, "PONG")
 	})
 
-	// query database for player
-	h.GET("/find", func(ctx context.Context, c *app.RequestContext) {
-
-		startTime := time.Now()
-
-		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-		defer cancel()
-
-		playerName := c.Query("name")
-		playerId := c.Query("uid")
-		//platform := c.Query("platform")
-
-		//
-		if playerName != "" && playerId != "" {
-			c.JSON(consts.StatusBadRequest, responses.Error(startTime, "name and uid params both used"))
-		}
-
-		if playerName == "" && playerId == "" {
-			c.JSON(consts.StatusBadRequest, responses.Error(startTime, "both name and uid params are empty"))
-		}
-
-		if playerId != "" && !utils.IsValidUUID(playerId) {
-			c.JSON(consts.StatusBadRequest, responses.Error(startTime, "invalid player uuid provided"))
-		}
-
-	})
-
 	h.POST("/index", ic.RequestHandler)
-
-	//h.GET("/player")
-
-	//h.POST("/update")
+	h.GET("/player", pc.RequestHandler)
+	h.GET("/search", ssc.RequestHandler)
+	h.POST("/update", uc.RequestHandler)
 }
