28df96fdd7e48c6c1b9d68fcb336dfb59224b763
Elias Sebastian <eeschmoelz@gmail.com>
2023-02-03 12:24:45 +0000

    feat: meili search functions

--- pkg/meili/meili.go
+++ pkg/meili/meili.go
@@ -3,6 +3,7 @@ package meili
 import (
 	"log"
 
+	"github.com/eliassebastian/r6index-api/cmd/api/models"
 	"github.com/meilisearch/meilisearch-go"
 )
 
@@ -14,6 +15,7 @@ func New() *MeiliSearchStore {
 	client := meilisearch.NewClient(meilisearch.ClientConfig{
 		Host:   "http://127.0.0.1:7700",
 		APIKey: "85d7c22efd3395fd5ed83c546f514585f6fa7e8ff4ed982fdf66c1b1cac24aef",
+		//APIKey: "qXPkGJcYpUQuGHUncsPpHxGsqwGygvXQUhUtzTATCbuBaHeYufTWtLddeYMHttFN",
 	})
 
 	return &MeiliSearchStore{
@@ -75,4 +77,19 @@ func (m *MeiliSearchStore) GetKeys() error {
 	log.Println(resp)
 	return e
 
+}
+
+// Search database based on platform and query provided
+func (m *MeiliSearchStore) Search(platform, query string) (*meilisearch.SearchResponse, error) {
+	return m.DB.Index(platform).Search(query, &meilisearch.SearchRequest{})
+}
+
+// Fetch complete player profile from database
+func (m *MeiliSearchStore) FetchPlayer(platform, uuid string, output *models.Player) error {
+	return m.DB.Index(platform).GetDocument(uuid, &meilisearch.DocumentQuery{}, output)
+}
+
+// Check whether player uuid exists in database
+func (m *MeiliSearchStore) FindPlayer(platform, uuid string, output *models.PlayerFound) error {
+	return m.DB.Index(platform).GetDocument(uuid, &meilisearch.DocumentQuery{Fields: []string{"profileId"}}, output)
 }
